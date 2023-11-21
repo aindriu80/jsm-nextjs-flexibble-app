a8e9fd77006edd922666de9ecc790a8fd0f173a9
aindriu80 <aindriu80@gmail.com>
2023-11-21 14:56:24 +0000

    Create Project - form

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,11 +1,17 @@
+import { redirect } from "next/navigation";
 import Modal from "@/components/Modal";
 import ProjectForm from "@/components/ProjectForm";
+import { getCurrentUser } from "@/lib/session";
 
-const createProject = () => {
+const createProject = async () => {
+  const session = await getCurrentUser();
+
+  if (!session?.user) redirect("/");
+
   return (
     <Modal>
       <h3 className="modal-head-text">Create a new project</h3>
-      <ProjectForm />
+      <ProjectForm type="create" session={session} />
     </Modal>
   );
 };
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+import { SessionInterface } from "@/common.types";
+import React, { ChangeEvent } from "react";
+import Image from "next/image";
+import FormField from "./FormField";
 
-const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+const ProjectForm = ({ type, session }) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  const handleStateChange = (fieldname: string, value: string) => {};
+
+  const image = null;
+
+  const form = {
+    image: "",
+    title: "",
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && "Choose a poster for your project"}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === "create"}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-30"
+            alt="Project Poster"
+            fill
+          />
+        )}
+      </div>
+
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexible"
+        setState={(value) => handleStateChange("title", value)}
+      />
+    </form>
+  );
 };
 
 export default ProjectForm;
